0c83372148406c3caca0af0be9b11847deadf747
laihang <15210970832@163.com>
2018-03-19 23:16:08 +0800

    贪心算法  删除k位数后  最大或最小

---
+++ src/main/java/com/collection/DeleteKNumbers.java
@@ -0,0 +1,30 @@
+package com.collection;
+
+public class DeleteKNumbers {
+
+
+	private static void deleteFunc(String str, int k) {
+
+		boolean flag;
+		for (int j = k; j > 0; j--) {
+			flag = false;
+			for (int i = 0; i < str.length() - 1; i++) {
+				//删除后最大<    删除后最小>
+				if (str.charAt(i) < str.charAt(i + 1)) {
+					str = str.replaceFirst(String.valueOf(str.charAt(i)), "");
+					flag = true;
+					break;
+				}
+			}
+			if (!flag) {
+				str = str.substring(0, str.length() - j);
+				break;
+			}
+		}
+		System.out.println(str);
+	}
+
+	public static void main(String[] args) {
+		deleteFunc("2319274", 3);
+	}
+}
